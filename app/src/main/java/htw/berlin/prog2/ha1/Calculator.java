86bf915d9f83854a89c2d1d37324170f9356f808
mar3a-sch <maria.schulz@student.htw-berlin.de>
2025-04-11 18:08:45 +0200

    Erster Roter Test Fix

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -74,16 +74,17 @@ public class Calculator {
      * @param operation "√" für Quadratwurzel, "%" für Prozent, "1/x" für Inversion
      */
     public void pressUnaryOperationKey(String operation) {
-        latestValue = Double.parseDouble(screen);
+        double value = Double.parseDouble(screen);
         latestOperation = operation;
         var result = switch(operation) {
             case "√" -> Math.sqrt(Double.parseDouble(screen));
-            case "%" -> Double.parseDouble(screen) / 100;
-            case "1/x" -> 1 / Double.parseDouble(screen);
+            case "%" -> value / 100;
+            case "1/x" -> value == 0 ? Double.POSITIVE_INFINITY : 1;
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if(screen.equals("NaN")) screen = "Error";
+        if(screen.equals("NaN") || screen.equals("Infinity")) screen = "Error";
+        if(screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
         if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
 
@@ -206,6 +207,8 @@ public class Calculator {
     public static void main(String[] args){
         Calculator c = new Calculator();
 
+
         c.DivisionMitNull();
+
     }
 }
