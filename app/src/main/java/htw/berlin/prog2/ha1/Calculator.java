d6345df97cf029179fec5b0c963d0eb4432883d0
mar3a-sch <maria.schulz@student.htw-berlin.de>
2025-04-17 08:47:31 +0200

    RICHTIG: Erster Roter Test

--- app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
+++ app/src/main/java/htw/berlin/prog2/ha1/Calculator.java
@@ -14,13 +14,6 @@ public class Calculator {
 
     private String latestOperation = "";
 
-   // private double lastOperand;
-
-   // private boolean repeatEqualsPressed = false;
-
-
-
-
     /**
      * @return den aktuellen Bildschirminhalt als String
      */
@@ -33,13 +26,13 @@ public class Calculator {
      * drücken kann muss der Wert positiv und einstellig sein und zwischen 0 und 9 liegen.
      * Führt in jedem Fall dazu, dass die gerade gedrückte Ziffer auf dem Bildschirm angezeigt
      * oder rechts an die zuvor gedrückte Ziffer angehängt angezeigt wird.
+     *
      * @param digit Die Ziffer, deren Taste gedrückt wurde
      */
     public void pressDigitKey(int digit) {
-        //repeatEqualsPressed = false;
-        if(digit > 9 || digit < 0) throw new IllegalArgumentException();
+        if (digit > 9 || digit < 0) throw new IllegalArgumentException();
 
-        if(screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
+        if (screen.equals("0") || latestValue == Double.parseDouble(screen)) screen = "";
 
         screen = screen + digit;
     }
@@ -65,10 +58,10 @@ public class Calculator {
      * Rechner in den passenden Operationsmodus versetzt.
      * Beim zweiten Drücken nach Eingabe einer weiteren Zahl wird direkt des aktuelle Zwischenergebnis
      * auf dem Bildschirm angezeigt. Falls hierbei eine Division durch Null auftritt, wird "Error" angezeigt.
+     *
      * @param operation "+" für Addition, "-" für Substraktion, "x" für Multiplikation, "/" für Division
      */
-    public void pressBinaryOperationKey(String operation)  {
-        //repeatEqualsPressed = false; //Tipp von Chat GPT; Hatte den Fehler meines Codes nicht gefunden, warum es nicht funktioniert
+    public void pressBinaryOperationKey(String operation) {
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
     }
@@ -78,23 +71,21 @@ public class Calculator {
      * Quadratwurzel, Prozent, Inversion, welche nur einen Operanden benötigen.
      * Beim Drücken der Taste wird direkt die Operation auf den aktuellen Zahlenwert angewendet und
      * der Bildschirminhalt mit dem Ergebnis aktualisiert.
+     *
      * @param operation "√" für Quadratwurzel, "%" für Prozent, "1/x" für Inversion
      */
     public void pressUnaryOperationKey(String operation) {
-        //double value = Double.parseDouble(screen);
         latestValue = Double.parseDouble(screen);
         latestOperation = operation;
-        var result = switch(operation) {
+        var result = switch (operation) {
             case "√" -> Math.sqrt(Double.parseDouble(screen));
-            case "%" ->  Double.parseDouble(screen) / 100; //value / 100;
-            case "1/x" ->  1 / Double.parseDouble(screen); //value == 0 ? Double.POSITIVE_INFINITY : 1;
+            case "%" -> Double.parseDouble(screen) / 100;
+            case "1/x" -> 1 / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
         screen = Double.toString(result);
-        if(screen.equals("NaN") || screen.equals("Infinity")) screen = "Error";
-        //if(screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
-
+        if (screen.equals("NaN")) screen = "Error";
+        if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
 
     }
 
@@ -106,7 +97,7 @@ public class Calculator {
      * Beim zweimaligem Drücken, oder wenn bereits ein Trennzeichen angezeigt wird, passiert nichts.
      */
     public void pressDotKey() {
-        if(!screen.contains(".")) screen = screen + ".";
+        if (!screen.contains(".")) screen = screen + ".";
     }
 
     /**
@@ -130,30 +121,16 @@ public class Calculator {
      * und das Ergebnis direkt angezeigt.
      */
     public void pressEqualsKey() {
-
-       /* double current = Double.parseDouble(screen);
-
-        if (latestOperation.equals("")) return;
-
-        if (repeatEqualsPressed) {
-            current = lastOperand;
-        } else {
-            lastOperand = current;
-            repeatEqualsPressed = true;
-        }
-        */
-        var result = switch(latestOperation) {
-            case "+" -> latestValue + Double.parseDouble(screen); //current;
-            case "-" -> latestValue - Double.parseDouble(screen); //current;
-            case "x" -> latestValue * Double.parseDouble(screen); //current;
-            case "/" -> latestValue / Double.parseDouble(screen); //current == 0 ? Double.POSITIVE_INFINITY : latestValue / current;
+        var result = switch (latestOperation) {
+            case "+" -> latestValue + Double.parseDouble(screen);
+            case "-" -> latestValue - Double.parseDouble(screen);
+            case "x" -> latestValue * Double.parseDouble(screen);
+            case "/" -> latestValue / Double.parseDouble(screen);
             default -> throw new IllegalArgumentException();
         };
-
-        //latestValue = result;
         screen = Double.toString(result);
-        if(screen.equals("Infinity")) screen = "Error";
-        if(screen.endsWith(".0")) screen = screen.substring(0,screen.length()-2);
-        if(screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
+        if (screen.equals("Infinity")) screen = "Error";
+        if (screen.endsWith(".0")) screen = screen.substring(0, screen.length() - 2);
+        if (screen.contains(".") && screen.length() > 11) screen = screen.substring(0, 10);
     }
 }
